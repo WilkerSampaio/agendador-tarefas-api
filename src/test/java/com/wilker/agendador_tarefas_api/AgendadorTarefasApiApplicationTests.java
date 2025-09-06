a525e61c59c1cf9af76c3c392af6cb669e105881
Wilker <neoxextreme@gmail.com>
2025-09-06 13:14:30 -0400

    Primeiro commit

---
+++ src/test/java/com/wilker/agendador_tarefas_api/AgendadorTarefasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilker.agendador_tarefas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
