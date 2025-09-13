0ef3fcc2500c380c87cbdf9d0b3c1ad8dac89f4b
Wilker <neoxextreme@gmail.com>
2025-09-12 20:53:39 -0400

    trabalhando com record

--- src/main/java/com/wilker/agendador_tarefas_api/infrastructure/dto/in/TarefasDTORequest.java
+++ src/main/java/com/wilker/agendador_tarefas_api/infrastructure/dto/in/TarefasDTORequest.java
@@ -2,26 +2,16 @@ package com.wilker.agendador_tarefas_api.infrastructure.dto.in;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.wilker.agendador_tarefas_api.infrastructure.enums.StatusNotificacaoEnum;
-import lombok.*;
-
 import java.time.LocalDateTime;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@Builder
-public class TarefasDTORequest {
-
-    private String id;
-    private String nomeTarefa;
-    private String descricao;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
-    private LocalDateTime dataCriacao;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
-    private LocalDateTime dataEvento;
-    private String emailUsuario;
-    private LocalDateTime dataAlteracao;
-    private StatusNotificacaoEnum statusNotificacaoEnum;
+public record TarefasDTORequest(String id,
+                                String nomeTarefa,
+                                String descricao,
+                                @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+                                LocalDateTime dataCriacao,
+                                @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss") LocalDateTime dataEvento,
+                                String emailUsuario,
+                                LocalDateTime dataAlteracao,
+                                StatusNotificacaoEnum statusNotificacaoEnum) {
 
 }
